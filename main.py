b7d125d5a4bd058a596f2d115e77a04660c32d78
zArby23 <lic.jdavid23@gmail.com>
2023-10-03 16:20:59 -0500

    Imported MessageCreate function, added a prescence, changed "on_message_create" function

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from interactions import Client, Intents, Status, listen
+from interactions.api.events import MessageCreate
 import token_1
 
-bot = Client(intents=Intents.DEFAULT,status=Status.IDLE)
+bot = Client(intents=Intents.DEFAULT,
+             status=Status.IDLE,
+             activity="WIP")
 
 @listen()
 async def on_ready():
     print("Baiheng's ready!")
 
-@listen()
-async def on_message_create(event):
+@listen(MessageCreate)
+async def on_message_create(event: MessageCreate):
     print(f"Message received: {event.message.content}")
 
 bot.start(f"{token_1.token_}")
